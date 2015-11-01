9b6ba78f627a6436ada8fb8dc823e2f0070b540f
Breno Alves <breno.wd@gmail.com>
2015-11-01 19:17:41 -0200

    get photo library

--- client/views/camfind.js
+++ client/views/camfind.js
@@ -1,6 +1,140 @@
 Template.camfind.events({
   'click #cam-find': function(event, template) {
-      callCamFind();
+         IonActionSheet.show({
+    buttons: [
+    { text: 'Take Photo' },
+    { text: 'Choose from Library' },
+    ],
+    cancelText: 'Cancel',
+
+    cancel: function() {
+      console.log('Cancelled!');
+    },
+
+    buttonClicked: function(index) {
+      var options = {
+        width: 1024,
+        height: 768,
+        quality: 75
+      }
+
+      switch (index) {
+        case 1:
+          //options.push({ sourceType: Camera.PictureSourceType.PHOTOLIBRARY });
+
+          console.log('pega foto da lib');
+
+                var optionsLib = {  
+                  width: 1024,
+                  height: 768,
+                  quality: 75,
+                  sourceType: Camera.PictureSourceType.PHOTOLIBRARY
+                }
+                
+
+                MeteorCamera.getPicture(optionsLib, function(err, data) {  
+
+                  if (data) {
+                          IonLoading.show();
+
+                          // start uploading
+                          Meteor.call('amazons3upload', data, function(error, result){
+                            console.log(result);
+
+                            // get first contact with camfind
+                            Meteor.call('camfindGetToken', result, function(error, result) {
+                              if (!error && result.statusCode == 200) {
+                                console.log("----camfindGetToken----");
+                                console.log(result)
+
+                                // get image response
+                                Meteor.call('camfindGetResponse', result.data.token, function(error, result) {
+                                  console.log("----camfindGetResponse----");
+                                  console.log(result);
+                                  $('#manualInputCamFind').val(result.data.name);
+                                  $('#manualSubmitCamFind').trigger('click');
+
+                                // if (result.status == "completed") {
+                                //   IonLoading.hide();
+                                //   //callback(result.name);
+                                //   console.log(result.name);
+                                //   return result.name;
+                                // } else {
+                                //
+                                //   console.log('nao completadooooooooooooooo aindaaaa');
+                                //   console.log(result)
+                                //
+                                //   initiateCamfind(result);
+                                // }
+                                })
+                              }
+                            });
+                          })
+                  }
+
+
+
+                });
+
+
+          break;
+        case 0:
+          //options.push({ sourceType: Camera.PictureSourceType.PHOTOLIBRARY });
+
+          console.log('bate foto');
+
+            MeteorCamera.getPicture(options, function(err, data) {
+              if (data) {
+                IonLoading.show();
+
+                // start uploading
+                Meteor.call('amazons3upload', data, function(error, result){
+                  console.log(result);
+
+                  // get first contact with camfind
+                  Meteor.call('camfindGetToken', result, function(error, result) {
+                    if (!error && result.statusCode == 200) {
+                      console.log("----camfindGetToken----");
+                      console.log(result)
+
+                      // get image response
+                      Meteor.call('camfindGetResponse', result.data.token, function(error, result) {
+                        console.log("----camfindGetResponse----");
+                        console.log(result);
+                        $('#manualInputCamFind').val(result.data.name);
+                        $('#manualSubmitCamFind').trigger('click');
+
+                      // if (result.status == "completed") {
+                      //   IonLoading.hide();
+                      //   //callback(result.name);
+                      //   console.log(result.name);
+                      //   return result.name;
+                      // } else {
+                      //
+                      //   console.log('nao completadooooooooooooooo aindaaaa');
+                      //   console.log(result)
+                      //
+                      //   initiateCamfind(result);
+                      // }
+                      })
+                    }
+                  });
+                })
+              }
+            });
+
+          break;
+        default:
+          break;
+      }
+
+      
+      return true;
+    }
+
+
+
+  });
   },
 
   'click #manualSubmitCamFind': function(e, template) {
@@ -47,78 +181,3 @@ Template.camfind.helpers({
     return Session.get('camfindphoto');
   }
 })
-
-
-function callCamFind() {
-  IonActionSheet.show({
-    buttons: [
-    { text: 'Take Photo' },
-    { text: 'Choose from Library' },
-    ],
-    cancelText: 'Cancel',
-
-    cancel: function() {
-      console.log('Cancelled!');
-    },
-
-    buttonClicked: function(index) {
-      var options = {
-        width: 1024,
-        height: 768,
-        quality: 75
-      }
-
-      switch (index) {
-        case 1:
-          options.push({ sourceType: Camera.PictureSourceType.PHOTOLIBRARY })
-          break;
-        default:
-          break;
-      }
-
-      MeteorCamera.getPicture(options, function(err, data) {
-        if (data) {
-          IonLoading.show();
-
-          // start uploading
-          Meteor.call('amazons3upload', data, function(error, result){
-            console.log(result);
-
-            // get first contact with camfind
-            Meteor.call('camfindGetToken', result, function(error, result) {
-              if (!error && result.statusCode == 200) {
-                console.log("----camfindGetToken----");
-                console.log(result)
-
-                // get image response
-                Meteor.call('camfindGetResponse', result.data.token, function(error, result) {
-                  console.log("----camfindGetResponse----");
-                  console.log(result);
-                  $('#manualInputCamFind').val(result.data.name);
-                  $('#manualSubmitCamFind').trigger('click');
-
-                // if (result.status == "completed") {
-                //   IonLoading.hide();
-                //   //callback(result.name);
-                //   console.log(result.name);
-                //   return result.name;
-                // } else {
-                //
-                //   console.log('nao completadooooooooooooooo aindaaaa');
-                //   console.log(result)
-                //
-                //   initiateCamfind(result);
-                // }
-                })
-              }
-            });
-          })
-        }
-      });
-      return true;
-    }
-
-
-
-  });
-}
