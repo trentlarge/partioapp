08b8c915885071c7c1ae1cd9a4e8055ba1620b0f
Lucas Bassetti <lucas@MacBook-Pro-de-Lucas.local>
2015-11-06 11:53:23 -0200

    Share improved.

--- client/views/Lend/resultsCamFind.js
+++ client/views/Lend/resultsCamFind.js
@@ -1,22 +1,11 @@
-//var RentingTimeSpan; //ONE_DAY, ONE_WEEK, ONE_MONTH, FOUR_MONTHS
-//var RentingOneDayPercentage;
-//var RentingOneWeekPercentage;
-//var RentingOneMonthPercentage;
-//var RentingFourMonthsPercentage;
-//var RentingPartioSharePercentage;
-//var RentingStripeFeePercent;
-//var RentingStripeAdditionalFee;
-//var RentingAmazonPrice;
-//var RentingAmazonRentalPrice;
-//var RentingFinalPrice;
 
 // RENDERED
 
-Template.resultsCamFind.rendered = function() {
-    //Session.set('allResults', true);
-    //Session.set('scanResult', false);
+//Template.resultsCamFind.rendered = function() {
+//    Session.set('allResults', true);
+//    Session.set('scanResult', false);
 //    console.log(Session.get('allResults'));
-}
+//}
 
 // HELPERS
 
@@ -129,93 +118,9 @@ Template.resultsCamFind.events({
 });
 
 // DESTROYED
-
-Template.resultsCamFind.destroyed = function() {
-    // Session.set('scanResult', null);
-    // Session.set('allResults', null);
-}
-
-
-//function CalculateRentingCharges()
-//{
-//  if(RentingTimeSpan == 'ONE_DAY')
-//  {
-//    RentingFinalPrice = parseFloat((RentingOneDayPercentage/100) * RentingAmazonPrice);
-//  }
-//  else if(RentingTimeSpan == 'ONE_WEEK')
-//  {
-//    RentingFinalPrice = parseFloat((RentingOneWeekPercentage/100) * RentingAmazonPrice);
-//    console.log('RentingOneWeekPercentage: ' + RentingFinalPrice);
 //
-//    RentingFinalPrice = parseFloat(RentingFinalPrice * 7);
-//    console.log('Pricex7: ' + RentingFinalPrice);
-//  }
-//  else if(RentingTimeSpan == 'ONE_MONTH')
-//  {
-//    RentingFinalPrice = parseFloat((RentingOneMonthPercentage/100) * RentingAmazonPrice);
-//    RentingFinalPrice = parseFloat(RentingFinalPrice * 30);
-//  }
-//  else if(RentingTimeSpan == 'FOUR_MONTHS')
-//  {
-//    RentingFinalPrice = parseFloat((RentingFourMonthsPercentage/100) * RentingAmazonPrice);
-//    RentingFinalPrice = parseFloat(RentingFinalPrice * 30 * 4);
-//  }
-//
-//  console.log('RentingTimeSpan: ' + RentingTimeSpan);
-//  console.log('RentingAmazonPrice: ' + RentingAmazonPrice);
-//  console.log('RentingAmazonRentalPrice: ' + RentingAmazonRentalPrice);
-//
-//  RentingFinalPrice += parseFloat((RentingStripeFeePercent/100) * RentingFinalPrice);
-//  console.log('RentingStripeFeePercent: ' + RentingFinalPrice);
-//  RentingFinalPrice += parseFloat(RentingStripeAdditionalFee);
-//  console.log('RentingStripeAdditionalFee: ' + RentingFinalPrice);
-//
-//  RentingFinalPrice += parseFloat((RentingPartioSharePercentage/100) * RentingFinalPrice);
-//  console.log('RentingPartioSharePercentage: ' + RentingFinalPrice);
-//
-//  if(RentingFinalPrice > RentingAmazonRentalPrice)
-//  {
-//    RentingFinalPrice =  parseFloat(RentingAmazonRentalPrice - (RentingAmazonRentalPrice * 0.1));
-//  }
-//
-//  RentingFinalPrice = Math.round(Number((RentingFinalPrice).toFixed(1))).toFixed(2);
-//  console.log('RentingFinalPrice: ' + Math.round(RentingFinalPrice));
-//
-//
-//}
-//
-//function GetRentingPercentages(strRentingTimeSpan)
-//{
-//  RentingAmazonPrice = parseFloat(Session.get('priceValue'));
-//  RentingTimeSpan = strRentingTimeSpan;
-//  RentingOneDayPercentage = 2;
-//  RentingOneWeekPercentage = 1;
-//  RentingOneMonthPercentage = 0.5;
-//  RentingFourMonthsPercentage = 0.25;
-//
-//  RentingPartioSharePercentage = 10;
-//
-//  RentingStripeFeePercent = 2.9;
-//  RentingStripeAdditionalFee = 0.3;
-//
-//  //test value
-//  RentingAmazonRentalPrice = 100.0;
-//
-//  CalculateRentingCharges();
-//}
-//
-//function ClearRentingValue()
-//{
-//  RentingFinalPrice = 0.0;
+//Template.resultsCamFind.destroyed = function() {
+//     Session.set('scanResult', null);
+//     Session.set('allResults', null);
 //}
 
-// function ClearData()
-// {
-//   console.log('ClearData');
-//   RentingFinalPrice = null;
-//   Session.set('scanResult', null);
-//   Session.set('allResults', null);
-//   Session.set('priceValue', null);
-//   Session.set('userPrice', null);
-//   Session.set('priceValue', null);
-// }
