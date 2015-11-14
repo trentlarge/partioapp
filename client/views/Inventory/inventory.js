5fd325c18bbdd8d16e8770b24f5bc7ebcaa7b01b
Clayton Marinho <claytonmarinho@gmail.com>
2015-11-13 22:55:09 -0200

    bytitle

--- client/views/Inventory/inventory.js
+++ client/views/Inventory/inventory.js
@@ -109,56 +109,6 @@ Template.inventory.events({
 
 })
 
-Template.inventoryDetail.events({
-  'click .features': function(e, template) {
-      
-      var features = $('.features');
-      var featureDetails = $('.features-details');
-      
-        if(featureDetails.hasClass('hidden')){
-            featureDetails.removeClass('hidden');
-            features.find('.chevron-icon').removeClass('ion-chevron-right').addClass('ion-chevron-down');
-        }
-        else {
-            featureDetails.addClass('hidden');
-            features.find('.chevron-icon').removeClass('ion-chevron-down').addClass('ion-chevron-right');
-        }
-      
-  },
-  'click #editSave': function(e, template) {
-    console.log("saving");
-
-    var dayPrice = parseInt(template.find('.dayPrice').value, 10),
-        weekPrice = parseInt(template.find('.weekPrice').value, 10),
-        monthPrice = parseInt(template.find('.monthPrice').value, 10),
-        semesterPrice = parseInt(template.find('.semesterPrice').value, 10);
-
-    if(dayPrice < 0.5 || weekPrice < 0.5 || monthPrice < 0.5 || semesterPrice < 0.5)
-    {
-      showInvalidPopUp('Invalid Inputs', 'Please enter a valid price.');
-      return false;
-    }
-
-    if(dayPrice > 100000 || weekPrice > 100000 || monthPrice > 100000|| semesterPrice > 100000)
-    {
-      showInvalidPopUp('Invalid Inputs', 'Please enter a Price less than 100000.');
-      return false;
-    }
-      
-     var editedPrices = {   
-        "day": template.find('.dayPrice').value,
-        "week": template.find('.weekPrice').value,
-        "month": template.find('.monthPrice').value,
-        "semester": template.find('.semesterPrice').value,
-     }
-
-    var edited = template.find('.semesterPrice').value;
-    var description = template.find('.fieldDescriptionLend').value;
-      
-    Products.update({_id: this._id}, {$set: {customPrice: edited, rentPrice: editedPrices, description: description}});
-    Session.set('editMode', false);
-  }
-});
 
 function showInvalidPopUp(strTitle, strMessage)
 {
@@ -178,28 +128,3 @@ function showInvalidPopUp(strTitle, strMessage)
 }
 
 var bookID;
-Template.inventoryDetail.helpers({
-  getCategoryIcon: function() {
-      return Categories.getCategoryIconByText(this.category);
-  },
-  editMode: function() {
-
-    var ConnectionObj = Connections.findOne({'productData._id': this._id});
-
-    if(ConnectionObj)
-    {
-      var ConnectionStatus = ConnectionObj.state;
-
-      if(ConnectionStatus != "DONE")
-      {
-        //Check if book is in RENTING mode and disable edit option
-        return false;
-      }
-    }
-
-    return Session.get('editMode') ? true : false;
-  },
-  manualEntry: function() {
-    return (this.manualEntry) ? true : false;
-  }
-})
--- server/collectionHooks.js
+++ server/collectionHooks.js
@@ -1,23 +1,72 @@
-Products.after.insert(function(userId, product) {
+Products.after.insert(function(userId, doc) {
+	refreshSearch(userId, doc)
+})
+
+Products.before.update(function(userId, doc) {
+	// Products.update({ _id: doc._id },
+	// 								{ $set:{ lastSearchId: doc.searchId }})
+})
+
+Products.after.update(function (userId, doc) {
+		console.log(' depoisssss--------------------------------------')
+		console.log(doc);
+	refreshSearch(userId, doc)
+});
+
+// Products.before.delete(function (userId, doc) {
+//   refreshSearch(userId, doc)
+// });
+
+
+function refreshSearch(userId, product){
 	var ownerData = Meteor.users.findOne(userId);
-	var existingProduct = Search.findOne({"uniqueId": product.uniqueId })
-	if (existingProduct) {
+	var existingSearch = Search.findOne({title: product.title })
+	if (existingSearch) {
 
-		var updatedAuthors = existingProduct.authors+', '+ownerData.profile.name;
-		console.log(updatedAuthors);
+		//already have this search id
+		if(product.searchId == existingSearch._id){
+			return false;
 
-		Search.update({ _id: existingProduct._id },
-									{ $inc: {qty: 1},
-										$set: {authors: updatedAuthors }});
+		} else {
+			var search_id =  existingSearch._id;
+			Products.update({ _id: product._id },
+											{ $set:{ searchId: search_id  }})
+			updateAuthors(search_id);
+		}
 	} else {
 		var newSearch = {
-			uniqueId: product.uniqueId,
 			image: product.image,
 			title: product.title,
-			authors: ownerData.profile.name,
-			qty: 1,
-			productUniqueId: product._id
+			authors: null,
+			qty: 0,
 		}
-		Search.insert(newSearch);
+
+		Search.insert(newSearch, function(err, docInserted){
+			var search_id =  docInserted;
+			Products.update({ _id: product._id },
+											{ $set:{ searchId: search_id  }})
+			updateAuthors(search_id);
+		});
 	}
-})
+
+	return true;
+}
+
+function updateAuthors(searchId){
+	var products = Products.find({ searchId: searchId }).fetch();
+	var bufferOwner = []
+
+	for (var i = 0; i < products.length; i++) {
+		var owner_ = Meteor.users.findOne(products[i].ownerId);
+		if(owner_){
+			bufferOwner.push(owner_.profile.name);
+		}
+	}
+
+	var owners = bufferOwner.join(', ');
+	if(owners.length > 0){
+		Search.update({_id: searchId },
+									{ $set: { authors: owners,
+									 	qty: bufferOwner.length }})
+	}
+}
