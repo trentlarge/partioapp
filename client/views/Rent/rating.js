358f59ee1bdcb404f285a24c576179090faf2283
Lucas Bassetti <lucas@MacBook-Pro-de-Lucas.local>
2015-11-18 19:05:35 -0200

    Rating improved.

--- client/views/Rent/rating.js
+++ client/views/Rent/rating.js
@@ -5,25 +5,27 @@ Template.rating.helpers({
   avgRating: function(userId) {
       
     if(this.rating) {
-        if(this.rating.length > 1) {
+        if(this.rating.length >= 1) {
             var ratingArray = this.rating;
             var totalCount = ratingArray.length;
             var sum = _.reduce(ratingArray, function(memo, num) {
-              return (Number(memo) + Number(num))/totalCount;
+                console.log(Number(memo));
+                console.log(Number(num));
+              return (Number(memo) + Number(num));
             });
-            return parseFloat(sum).toFixed(1);
+            return parseFloat(sum/totalCount).toFixed(1);
         }
     }
       
     if(Meteor.users.findOne(userId)){
         if (Meteor.users.findOne(userId).profile.rating) {
-          if (Meteor.users.findOne(userId).profile.rating.length > 1) {
+          if (Meteor.users.findOne(userId).profile.rating.length >= 1) {
             var ratingArray = Meteor.users.findOne(userId).profile.rating;
             var totalCount = ratingArray.length;
             var sum = _.reduce(ratingArray, function(memo, num) {
-              return (Number(memo) + Number(num))/totalCount;
+              return Number(memo) + Number(num);
             });
-            return parseFloat(sum).toFixed(1);
+            return parseFloat(sum/totalCount).toFixed(1);
           }
         } else {
           return "0.0";
