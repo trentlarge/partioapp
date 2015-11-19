e3e222c1876032c6db4c34939f591a2781e444b2
Breno Alves <breno.wd@gmail.com>
2015-11-19 06:55:16 -0200

    template change password

--- server/server.js
+++ server/server.js
@@ -34,8 +34,6 @@ Kadira.connect('qhAvzzmgKeHaZ9rd9', '338e5eb7-842c-47f5-bfe7-7a4d3b9c0607');
     };
     Accounts.emailTemplates.verifyEmail.html = function(user, url) {
 
-
-
       var body =
       '<!DOCTYPE html>\
               <html>\
@@ -79,14 +77,51 @@ Kadira.connect('qhAvzzmgKeHaZ9rd9', '338e5eb7-842c-47f5-bfe7-7a4d3b9c0607');
 
             return body;
 
-      // return "Hello there! \n\n" +
-      // "Welcome aboard partiO! \n" +
-      // "The things you own end up making money for you! Sounds familiar? Er..nevermind! To make this happen, it all starts with one link. \n" +
-      // "The one below. Click to verify and get sharing! \n\n" +
-      // url + "\n\n" +
-      // "For any queries or support, feel free to contact partio.missioncontrol@gmail.com \n" +
-      // "Best\n" +
-      // "partiO team"
+    };
+
+    Accounts.emailTemplates.resetPassword.html = function(user, url) {
+
+      var body =
+      '<!DOCTYPE html>\
+              <html>\
+                  <head>\
+                      <title>Partio</title>\
+                      <meta http-equiv="Content-Type" content="text/html; charset=utf-8">\
+                      <style>\
+                      a {\
+                          color:#95cbab;\
+                      }\
+                      </style>\
+                  </head>\
+                  <body>\
+                      <table width="750" bgcolor="#f6f6f6">\
+                          <tr height="373">\
+                              <td><img src="http://partio.cloudservice.io/img/template_cabecalho.jpg" /></td>\
+                          </tr>\
+                          <tr>\
+                              <td>\
+                                  <div style="width:640px;font-family:arial; tex-align:left; margin-left:50px;color:#999">\
+                                      <h1 style="color:#263238;font-size:40px">Hello there!</h1>\
+                                      <p style="font-size:20px;line-height:38px;">Hello '+user+'Breno Alves'',<br />\
+                                      To reset your password, simply click the link below.\
+                                       '+url+'\
+                                       <br />Thanks.<br />\
+                                      partiO team\
+                                      </p>\
+                                  </div>\
+                              </td>\
+                          </tr>\
+                          <tr height="262">\
+                              <td><img src="http://partio.cloudservice.io/img/template_rodape.jpg" /></td>\
+                          </tr>\
+                      </table>\
+                  </body>\
+              </html>';
+
+
+
+            return body;
+
     };
 
     // Stripe = StripeSync('sk_test_RBrpczGtVbB1tSaG66gglMTH');
