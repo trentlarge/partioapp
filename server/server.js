347bf59e2f9d3e8dddd59d83b30cd13c2ce8a121
Breno Alves <breno.wd@gmail.com>
2015-11-19 06:30:19 -0200

    change template email

--- server/server.js
+++ server/server.js
@@ -32,54 +32,61 @@ Kadira.connect('qhAvzzmgKeHaZ9rd9', '338e5eb7-842c-47f5-bfe7-7a4d3b9c0607');
     Accounts.emailTemplates.verifyEmail.subject = function(user) {
       return 'Welcome to partiO!';
     };
-    Accounts.emailTemplates.verifyEmail.text = function(user, url) {
+    Accounts.emailTemplates.verifyEmail.html = function(user, url) {
 
 
 
-      // var body =
-      // '<!DOCTYPE html>\
-      //         <html>\
-      //             <head>\
-      //                 <title>Partio</title>\
-      //                 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">\
-      //                 <style>\
-      //                 a {\
-      //                     color:#95cbab;\
-      //                 }\
-      //                 </style>\
-      //             </head>\
-      //             <body>\
-      //                 <table width="750" bgcolor="#F8EFE0">\
-      //                     <tr height="373">\
-      //                         <td><img src="http://partio.cloudservice.io/img/template_cabecalho.jpg" /></td>\
-      //                     </tr>\
-      //                     <tr>\
-      //                         <td>\
-      //                             <div style="width:640px;font-family:arial; tex-align:left; margin-left:50px;color:#858380">\
-      //                                 <h1 style="color:#2ea570;font-size:40px">Subject</h1>\
-      //                                 <p style="font-size:20px;line-height:38px;">O texto vai aqui</p>\
-      //                             </div>\
-      //                         </td>\
-      //                     </tr>\
-      //                     <tr height="262">\
-      //                         <td><img src="http://partio.cloudservice.io/img/template_rodape.jpg" /></td>\
-      //                     </tr>\
-      //                 </table>\
-      //             </body>\
-      //         </html>';
-      //
-      //
-      //
-      //       return body;
+      var body =
+      '<!DOCTYPE html>\
+              <html>\
+                  <head>\
+                      <title>Partio</title>\
+                      <meta http-equiv="Content-Type" content="text/html; charset=utf-8">\
+                      <style>\
+                      a {\
+                          color:#95cbab;\
+                      }\
+                      </style>\
+                  </head>\
+                  <body>\
+                      <table width="750" bgcolor="#f6f6f6">\
+                          <tr height="373">\
+                              <td><img src="http://partio.cloudservice.io/img/template_cabecalho.jpg" /></td>\
+                          </tr>\
+                          <tr>\
+                              <td>\
+                                  <div style="width:640px;font-family:arial; tex-align:left; margin-left:50px;color:#999">\
+                                      <h1 style="color:#263238;font-size:40px">Hello there!</h1>\
+                                      <p style="font-size:20px;line-height:38px;">Welcome aboard partiO!\
+                                      The things you own end up making money for you! Sounds familiar? Er..nevermind! To make this happen, it all starts with one link.\
+                                      The one below. Click to verify and get sharing\
+                                       '+url+'\
+                                       For any queries or support, feel free to contact partio.missioncontrol@gmail.com\
+                                      Best\
+                                      partiO team\
+                                      </p>\
+                                  </div>\
+                              </td>\
+                          </tr>\
+                          <tr height="262">\
+                              <td><img src="http://partio.cloudservice.io/img/template_rodape.jpg" /></td>\
+                          </tr>\
+                      </table>\
+                  </body>\
+              </html>';
 
-      return "Hello there! \n\n" +
-      "Welcome aboard partiO! \n" +
-      "The things you own end up making money for you! Sounds familiar? Er..nevermind! To make this happen, it all starts with one link. \n" +
-      "The one below. Click to verify and get sharing! \n\n" +
-      url + "\n\n" +
-      "For any queries or support, feel free to contact partio.missioncontrol@gmail.com \n" +
-      "Best\n" +
-      "partiO team"
+
+
+            return body;
+
+      // return "Hello there! \n\n" +
+      // "Welcome aboard partiO! \n" +
+      // "The things you own end up making money for you! Sounds familiar? Er..nevermind! To make this happen, it all starts with one link. \n" +
+      // "The one below. Click to verify and get sharing! \n\n" +
+      // url + "\n\n" +
+      // "For any queries or support, feel free to contact partio.missioncontrol@gmail.com \n" +
+      // "Best\n" +
+      // "partiO team"
     };
 
     // Stripe = StripeSync('sk_test_RBrpczGtVbB1tSaG66gglMTH');
