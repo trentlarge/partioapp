73844c4b4395553ed627b4577a3638d2fddb2636
Breno Alves <breno.wd@gmail.com>
2015-12-02 04:42:58 -0200

    not allow lending to yourself

--- server/server.js
+++ server/server.js
@@ -103,13 +103,39 @@ Meteor.startup(function() {
 
 // LISTING SEARCH ------------------------------
 SearchSource.defineSource('packages', function(searchText, options) {
+
+
+
+  var userProducts = Products.find({ownerId: Meteor.userId() }).fetch();
+
+  var searchIds = []
+
+  userProducts.map(function(prod){
+   searchIds.push(prod.searchId)
+  });
+
+
+  var user_id = Meteor.userId();
+
+  console.log('######### NOT IN');
+  //var result_products = Products.find({ownerId: user_id}, { searchId : 1, _id : 0}).fetch();
+  //console.log(result_products);
+  //console.log(user_id);
+  console.log(searchIds);
+
+  //db.search.find({ _id: { $nin: [ 'bwCTtntQjSkXGMFkx' ] } })
+
+
+
   var options = {sort: {isoScore: -1}, limit: 20};
   if(searchText) {
     var regExp = buildRegExp(searchText);
-    var selector = {$or: [{title: regExp},{category: regExp},{uniqueId: searchText}]};
+    var selector = {_id: { $nin: searchIds}, $or: [{title: regExp},{category: regExp},{uniqueId: searchText}]};
+    console.log('searchText');
     return Search.find(selector, options).fetch();
   } else {
-    return Search.find({}, options).fetch();
+    var selector = {_id: { $nin: searchIds}};
+    return Search.find(selector, options).fetch();
   }
 });
 
