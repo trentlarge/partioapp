5c9711a05bc955b9b2308395975be9121ecdd5b1
Lucas Bassetti <lucas@MacBook-Pro-de-Lucas.local>
2015-12-04 17:20:17 -0200

    Improve inventory/renting request design.

--- client/views/Inventory/Inventory_controller.js
+++ client/views/Inventory/Inventory_controller.js
@@ -39,7 +39,36 @@ InventoryController = RouteController.extend({
             },
             getCondition: function(conditionId) {
                 return Rating.getConditionByIndex(conditionId);
-            }
+            },
+            getRequestDate: function(requestDate) {
+					return formatDate(requestDate);
+			},
+			getStartDate: function(startDate) {
+					return formatDate(startDate);
+			},
+			getEndDate: function(endDate) {
+					return formatDate(endDate);
+			},
+			getTotalDays: function(totalDays) {
+				if( totalDays > 1 ) {
+					return totalDays + ' days';
+				}
+				return totalDays + ' day';
+			},      
+            getDaysLeft: function(startDate, endDate) {
+				 var diff;
+				 if($.now() > new Date(startDate).getTime()) {
+						 diff = new Date(endDate - $.now());
+				 } else {
+						 diff = new Date(endDate - startDate);
+				 }
+				 var daysLeft = Math.floor((diff/1000/60/60/24) + 1);
+
+				 if(daysLeft <= 1) {
+						 return daysLeft + ' day left'
+				 }
+				 return daysLeft + ' days left';
+			},
 		}
 	},
 	onAfterAction: function() {
--- client/views/Renting/Renting_controller.js
+++ client/views/Renting/Renting_controller.js
@@ -22,31 +22,60 @@ RentingController = RouteController.extend({
 
 	data: function() {
 		return {
-			toBeApproved: function() {
-		    return Connections.find({"requestor": Meteor.userId(), "state": "WAITING"}); //{$ne: "IN USE", $ne: "DONE"}})
-		  },
-		  toBePaid: function() {
-		    return Connections.find({"requestor": Meteor.userId(), "state": "PAYMENT"}); //{$ne: "IN USE", $ne: "DONE"}})
-		  },
-		  currentlyBorrowed: function() {
-		    return Connections.find({"requestor": Meteor.userId(), "state": "IN USE"}); //{$ne: "IN USE", $ne: "DONE"}})
-		  	//return Connections.find({"requestor": Meteor.userId(), "state": "DONE"});
-		  },
-		  dataExists: function() {
-		  	return (Connections.find({"requestor": Meteor.userId(), "state": {$ne: "IN USE"}}).count() || Connections.find({"requestor": Meteor.userId(), "state": "IN USE"}).count()) ? true : false;
-		  },
-		  isReturned: function() {
-		    return (Connections.find({"requestor": Meteor.userId(), "state": "RETURNED"}));
-		  },
-		  itemReturned: function() {
-		    return (Connections.find({"requestor": Meteor.userId(), "state": "RETURNED"})).count();
-		  },
-		  isTapping: function() {
-		      return Session.get('isTapping');
-		  },
-		  getProductCondition: function() {
-		      return Rating.getConditionByIndex(this.productData.conditionId);
-		  }
+            toBeApproved: function() {
+                return Connections.find({"requestor": Meteor.userId(), "state": "WAITING"}); //{$ne: "IN USE", $ne: "DONE"}})
+            },
+            toBePaid: function() {
+                return Connections.find({"requestor": Meteor.userId(), "state": "PAYMENT"}); //{$ne: "IN USE", $ne: "DONE"}})
+            },
+            currentlyBorrowed: function() {
+                return Connections.find({"requestor": Meteor.userId(), "state": "IN USE"}); //{$ne: "IN USE", $ne: "DONE"}})
+            //return Connections.find({"requestor": Meteor.userId(), "state": "DONE"});
+            },
+            dataExists: function() {
+                return (Connections.find({"requestor": Meteor.userId(), "state": {$ne: "IN USE"}}).count() || Connections.find({"requestor": Meteor.userId(), "state": "IN USE"}).count()) ? true : false;
+            },
+            isReturned: function() {
+                return (Connections.find({"requestor": Meteor.userId(), "state": "RETURNED"}));
+            },
+            itemReturned: function() {
+                return (Connections.find({"requestor": Meteor.userId(), "state": "RETURNED"})).count();
+            },
+            isTapping: function() {
+                return Session.get('isTapping');
+            },
+            getProductCondition: function(conditionId) {
+                return Rating.getConditionByIndex(conditionId);
+            },
+            getRequestDate: function(requestDate) {
+                    return formatDate(requestDate);
+            },
+            getStartDate: function(startDate) {
+                    return formatDate(startDate);
+            },
+            getEndDate: function(endDate) {
+                    return formatDate(endDate);
+            },
+            getTotalDays: function(totalDays) {
+                if( totalDays > 1 ) {
+                    return totalDays + ' days';
+                }
+                return totalDays + ' day';
+            },      
+            getDaysLeft: function(startDate, endDate) {
+                 var diff;
+                 if($.now() > new Date(startDate).getTime()) {
+                         diff = new Date(endDate - $.now());
+                 } else {
+                         diff = new Date(endDate - startDate);
+                 }
+                 var daysLeft = Math.floor((diff/1000/60/60/24) + 1);
+
+                 if(daysLeft <= 1) {
+                         return daysLeft + ' day left'
+                 }
+                 return daysLeft + ' days left';
+            },
 		};
 	},
 
