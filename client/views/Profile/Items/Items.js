6a7a73bd5f84f4acd0b4fa31c7074f0ddb7d52f8
Breno Alves <breno.wd@gmail.com>
2015-12-05 22:01:13 -0200

    Click profile and see all items lended by the user - progress

---
+++ client/views/Profile/Items/Items.js
@@ -0,0 +1,21 @@
+Template.items.events({
+  'click #requestProduct': function() {
+    console.log('requesting product...');
+    var ownerId = this.ownerId;
+    var productId = this._id;
+    //console.log(this);
+    //console.log(Products.findOne(this._id));
+    IonModal.open("requestRent", Products.findOne(this._id));
+  },
+
+  'click #moreInfo': function() {
+    console.log('display product info...');
+    //console.log(this._id)
+    ///var productId = this._id;
+    // var ownerId = this.ownerId;
+    // var productId = this._id;
+    // console.log(this);
+    //console.log(Products.findOne(this._id));
+    IonModal.open("productDetail", Products.findOne(this._id));
+  }
+})
