261777b16cbd465c7f16ae8a1a239092a15b09af
Clayton Marinho <claytonmarinho@gmail.com>
2015-12-15 02:38:50 -0200

    improving transactions

--- server/publish/transactions.js
+++ server/publish/transactions.js
@@ -1,3 +1,7 @@
 Meteor.publish("transactions", function() {
 	return Transactions.find({}, {});
 });
+
+Meteor.publish("myTransactions", function() {
+	return Transactions.find({ "_id": Meteor.user().profile.transactionsId });
+});
