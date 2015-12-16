38ba27af84ba865553f96f35a79475f818b42b68
Clayton Marinho <claytonmarinho@gmail.com>
2015-12-16 15:24:47 -0200

    [done] twilio refactor with private data

--- client/lib/partioCall.js
+++ client/lib/partioCall.js
@@ -13,97 +13,44 @@ app.model.PartioCall = (function () {
       var requestor = connection.requestor;
       var owner = connection.owner;
 
-      console.log('-aquiiiii-----')
-      console.log(requestor, owner);
-
       if(!requestor || !owner) {
         console.log('Expecting requestor and owner IDS'); return false;
-      }
-
-      var currentUser = Meteor.user();
-
-      // USER NOT CONFIGURED PHONE NUMBER
-      if(!currentUser || !currentUser.private.mobile) {
-        IonPopup.show({
-          title: 'Ops...',
-          template: '<div class="center dark">Your phone isn\'t configured yet. Please update your phone number.</div>',
-          buttons:
-          [{
-            text: 'OK',
-            type: 'button-energized',
-            onTap: function() {
-              IonPopup.close();
-              Router.go('/profile');
-            }
-          }]
-        });
-
-        return false;
-      }
-
-      var _from = currentUser.profile.mobile;
-      var _to = false;
-
-      //calling connect owner
-      if(currentUser._id == requestor){
-        var _to = connection.productData.ownerData.profile.mobile;
-
-      //calling connect requestor
-      } else if(currentUser._id == owner) {
-        var _to = connection.requestorData.profile.mobile;
-      }
-
-      console.log(_from, _to)
-
-      // DESTINATION NOT CONFIGURED HIM PHONE NUMMBER
-      if(!_to || _to.trim() == '') {
-        IonPopup.show({
-          title: 'Ops...',
-          template: '<div class="center dark">Sorry. The another part isn\'t configured him phone number yet. Please try chat. Thank you.</div>',
-          buttons:
-          [{
-            text: 'OK',
-            type: 'button-energized',
-            onTap: function() {
-              IonPopup.close();
-            }
-          }]
-        });
-
-        return false;
       }
 
       PartioLoad.show();
 
   		//CHECK NUMBER ON TWILIO API
-  		Meteor.call('twilioVerification', _from, function(error, result) {
-
-  			// IF GET SOME ERROR FROM TWILIO
+  		Meteor.call('twilioVerification', function(error, result) {
   			if(error) {
-  				console.log('>>>> twilio error');
-  				console.log(error);
-  				PartioLoad.hide();
+          if(error.error == 'no_number') {
+            PartioLoad.hide();
 
-  				IonPopup.show({
-  					title: 'Ops...',
-  					template: '<div class="center dark">Sorry, the service is unavailable at this moment. Please try again later. Thank you. ;)</div>',
-  					buttons:
-  					[{
-  						text: 'OK',
-  						type: 'button-energized',
-  						onTap: function() {
-  							IonPopup.close();
-  						}
-  					}]
-  				});
+    				IonPopup.show({
+    					title: 'Ops...',
+    					template: '<div class="center dark">'+error.reason+'</div>',
+    					buttons:
+    					[{
+    						text: 'OK',
+    						type: 'button-energized',
+    						onTap: function() {
+    							IonPopup.close();
+                  Router.go('/profile')
+    						}
+    					}]
+    				});
 
-  				return false;
+    				return false;
+
+          } else {
+    				PartioLoad.hide();
+            ShowNotificationMessage(error.reason)
+    				return false;
+          }
   			}
 
   			// TWILIO IS WORKING
   			if(result){
-
-  				//REGISTERING FIRST TIME
+          //REGISTERING FIRST TIME
   				if(result.statusCode == 200) {
             PartioLoad.hide();
 
@@ -111,22 +58,36 @@ app.model.PartioCall = (function () {
 
   					IonPopup.show({
   						title: 'Phone activation',
-  						template: '<div class="center dark">Please, answer call and digit your activation number: "'+result.data.validation_code+'". Press OK when done. Thank you.</div>',
+  						template: '<div class="center dark">Please, answer call and digit your activation number: "'+result.data.validation_code+'". Press OK when done and try to call again. Thank you.</div>',
   						buttons:
   						[{
   							text: 'OK',
   							type: 'button-energized',
   							onTap: function() {
                   IonPopup.close();
-                  PartioLoad.show();
-                  PartioCall.makeCall(_from, _to);
+                  //PartioLoad.show();
+                  //PartioCall.makeCall(_from, _to);
   							}
   						}]
   					});
 
   				//ALREADY REGISTRED
   				} else if(result.statusCode == 400) {
+
+            var currentUser = Meteor.user();
+
+            var _from = currentUser._id;
+            var _to = false;
+
+            if(_from == requestor){
+              var _to = owner;
+
+            } else if(_from == owner) {
+              var _to = requestor;
+            }
+
   					console.log('Twilio >>>>>>> phone already registered')
+            PartioLoad.show();
             PartioCall.makeCall(_from, _to);
   				}
   			}
@@ -134,13 +95,18 @@ app.model.PartioCall = (function () {
     },
 
     makeCall: function(_from, _to) {
-      Meteor.call('callTwilio', { from: _from, to: _to }, function(error, result){
+      Meteor.call('callTwilio', _from, _to, function(error, result){
         console.log('Twilio >>>> call callTwilio method >>>');
-        console.log(error); console.log(result);
 
         PartioLoad.hide();
 
-        if(!error) {
+        if(error){
+          ShowNotificationMessage(error.reason)
+          console.log(error);
+          return false;
+        }
+
+        if(result) {
           // NOT REGISTERED YET
           if(result.data.code == '21210') {
             IonPopup.show({
@@ -176,19 +142,6 @@ app.model.PartioCall = (function () {
               }]
             });
           }
-        } else {
-          IonPopup.show({
-            title: 'Ops...',
-            template: '<div class="center dark">Service API .</div>',
-            buttons:
-            [{
-              text: 'OK',
-              type: 'button-energized',
-              onTap: function() {
-                IonPopup.close();
-              }
-            }]
-          });
         }
       });
     }
