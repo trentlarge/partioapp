587200b4c3896adc89153faaaeeab4b2d6e8fc15
Breno Alves <breno.wd@gmail.com>
2015-12-28 18:36:39 -0200

    disable facebook

--- client/router.js
+++ client/router.js
@@ -93,22 +93,22 @@ Router.route('/contact', { name: 'contact', controller: 'ContactController'});
 Router.onBeforeAction(function(pause){
 	if(!Meteor.user()) {
 		this.render('login')
-		console.log('login');
+		//console.log('login');
 	} else {
 		if(Meteor.user().emails[0].address){
-			console.log('entrou aqui');
+			//console.log('entrou aqui');
 
 			if(Meteor.user().emails[0].verified) {
 				this.next();
-				Router.go('/profile')
-				console.log('next');
+				//Router.go('/profile')
+				//console.log('next');
 			} else {
 				this.render('profile')
-				console.log('profile1');
+				//console.log('profile1');
 			}
 		} else {
 			this.render('profile')
-			console.log('profile2');
+			//console.log('profile2');
 		}
 	}
 }, {except: ['emailverification', 'register', 'login', 'contact']} );
