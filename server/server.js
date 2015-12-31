1bc943a8d3165a6f843071ec59e5abd138ee99a5
Clayton Marinho <claytonmarinho@gmail.com>
2015-12-31 14:44:33 -0200

    changing img url

--- server/server.js
+++ server/server.js
@@ -47,7 +47,7 @@ Meteor.startup(function() {
                 <body>\
                     <table width="750" bgcolor="#f6f6f6">\
                         <tr height="373">\
-                            <td><img src="http://partio.cloudservice.io/img/template_cabecalho.jpg" /></td>\
+                            <td><img src="http://cloudservice.io/partio/template_cabecalho.jpg" /></td>\
                         </tr>\
                         <tr>\
                             <td>\
@@ -65,7 +65,7 @@ Meteor.startup(function() {
                             </td>\
                         </tr>\
                         <tr height="262">\
-                            <td><img src="http://partio.cloudservice.io/img/template_rodape.jpg" /></td>\
+                            <td><img src="http://cloudservice.io/partio/template_rodape.jpg" /></td>\
                         </tr>\
                     </table>\
                 </body>\
@@ -89,7 +89,7 @@ Meteor.startup(function() {
                 <body>\
                     <table width="750" bgcolor="#f6f6f6">\
                         <tr height="373">\
-                            <td><img src="http://partio.cloudservice.io/img/template_cabecalho.jpg" /></td>\
+                            <td><img src="http://cloudservice.io/partio/template_cabecalho.jpg" /></td>\
                         </tr>\
                         <tr>\
                             <td>\
@@ -105,7 +105,7 @@ Meteor.startup(function() {
                             </td>\
                         </tr>\
                         <tr height="262">\
-                            <td><img src="http://partio.cloudservice.io/img/template_rodape.jpg" /></td>\
+                            <td><img src="http://cloudservice.io/partio/template_rodape.jpg" /></td>\
                         </tr>\
                     </table>\
                 </body>\
