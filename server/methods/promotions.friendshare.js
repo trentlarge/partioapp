52627a0938fed99ee49858b308bc2dfc5c2a586e
Clayton Marinho <claytonmarinho@gmail.com>
2016-02-20 20:02:27 -0200

    inserting Parent code

--- server/methods/promotions.friendshare.js
+++ server/methods/promotions.friendshare.js
@@ -57,4 +57,75 @@ Meteor.methods({
       }
   	}, 1500);
   },
+
+  insertBestFriendCode: function(insertCode){
+    if(!insertCode){
+      return false;
+    }
+
+    var code = insertCode.trim();
+
+    if(!code.length == 6) {
+      return false;
+    }
+
+    var _user = Meteor.user();
+
+    // check if that is not the user own code
+    if(_user.private.promotions.friendShare.code != code){
+
+      // check if there is a user and he is verified
+      var _parent = Meteor.users.findOne({ $and: [ { 'private.promotions.friendShare.code': code }, { 'emails.0.verified': true }] });
+
+      if(_parent) {
+        var _userChildren = _user.private.promotions.friendShare.children;
+
+        if(!_userChildren || _userChildren.length < 1) {
+          _userChildren = [];
+        }
+
+        // check if parent is not children
+        if(!_userChildren.has(_parent._id)) {
+
+          // update user parent field
+          Meteor.users.update({"_id": _user._id }, {$set: { "private.promotions.friendShare.parent": _parent._id }}, function(error) {
+            if(error) {
+              return false;
+            }
+
+            // update parent childrends field
+            var _parentChildren = _parent.private.promotions.friendShare.children;
+
+            if(!_parentChildren || _parentChildren.length < 1) {
+              _parentChildren = [];
+            }
+
+            //check if user is not already children of parent
+            if(!_parentChildren.has(_user._id)) {
+              _parentChildren.push(_user._id);
+
+              Meteor.users.update({"_id": _parent._id }, {$set: { "private.promotions.friendShare.children": _parentChildren }}, function(error) {
+                if(error) {
+                  return false;
+                }
+                return true;
+              });
+
+            } else { // user is already children from that
+              return false;
+            }
+          });
+
+        } else { // trying to insert child like parent
+          return false;
+        }
+
+      } else { // no parent (user not found)
+        return false;
+      }
+
+    } else { // own code
+      return false;
+    }
+  }
 });
