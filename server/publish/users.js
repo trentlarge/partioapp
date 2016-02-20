73e6c0924e02ed208dd083676e0ab8bb962c7693
Lucas Bassetti <lucas@MBP-de-Lucas.home>
2016-02-20 21:19:41 -0200

    Create promotion screen.

--- server/publish/users.js
+++ server/publish/users.js
@@ -12,6 +12,14 @@ Meteor.publish("adminSearchUsers", function(text, limit) {
     });
 });
 
+Meteor.publish("userBestFriend", function(promoCode) {
+	return Users.find({ 'private.promotions.friendShare.code': promoCode }, { fields: { profile: 1, 'private.promotions': 1 }});
+});
+
+Meteor.publish("userFriends", function(friends) {
+	return Users.find({ _id: { $in: friends }}, { fields: { profile: 1 }});
+});
+
 Meteor.publish("searchSingleUser", function(idUser) {
 	return Users.find({ _id: idUser }, { fields: { emails: 1, profile: 1, private: 1, createdAt: 1 }});
 });
