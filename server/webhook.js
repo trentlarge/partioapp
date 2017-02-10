2fc424e837fd7308dabb36d460f6fc20d4d110cd
Lucas Bassetti <lucas@MBP-de-Lucas.home>
2017-02-10 01:32:23 -0200

    update webhook

--- server/webhook.js
+++ server/webhook.js
@@ -6,14 +6,19 @@ Meteor.startup(function() {
         var connection = Connections.findOne({ 'policy.id': event.policy.id });
 
         if(connection) {
-            var message = 'An claim was filed for ' + connection.policy.product.name;
+            var message = 'A claim was filed for ' + connection.policy.product.name;
 
             sendPush(connection.owner, message);
             sendNotification(connection.owner, connection.requestor, message, "info", connection._id);
-        }
 
-        this.response.statusCode = 200;
-        this.response.end()
+            this.response.statusCode = 200;
+            this.response.end()
+        }
+        else {
+            this.response.statusCode = 200;
+            this.response.end()
+        }
+        
     }, { where: "server" });
 
 });
